5f709105cb302c9ca7da555af129ad0fe3855502
Toby Lawrance <toby.lawrance@gmail.com>
2026-03-15 21:25:27 +0000

    Fix logic on Fill targets

--- src/EnergyBitch.ts
+++ src/EnergyBitch.ts
@@ -13,7 +13,7 @@ export var roleEnergyBitch = {
         {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN &&
+                    return ((structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
                 }
             }); 
